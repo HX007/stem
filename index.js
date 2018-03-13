5e293e57d4fb1876cbfe27a30ccf6d235fd7c487
HX007 <1178369212@qq.com>
2018-03-13 20:15:49 +0800

    数据插入 03.13 晚20:15

--- index.js
+++ index.js
@@ -3,157 +3,160 @@ for(var a=0;a<$(".activeText").find("i").length;a++){
 	$(".activeText").find("i")[a].style.backgroundPosition = -16*a+"px"+" 0px"
 }
 
+function luoji(){
 
-//初始化轮播图
-$(".lunbo").find(".bigBox .box").hide()//隐藏所有的,boxList
+	//初始化轮播图
+	$(".lunbo").find(".bigBox .box").hide()//隐藏所有的,boxList
 
-for(var a=0;a<$(".lunbo").length;a++){
-	var $cont = $(".lunbo").eq(a).find(".bigBox .box").length
-	//初始化span点击小方块
-	for(var b=0;b<$cont;b++){
-		var $createSpan = $("<span></span>")//创建
-		$createSpan.attr("index",b)	//设置非法属性
-		$createSpan.appendTo($(".lunbo").eq(a).find(".dot"))
+	for(var a=0;a<$(".lunbo").length;a++){
+		var $cont = $(".lunbo").eq(a).find(".bigBox .box").length
+		//初始化span点击小方块
+		for(var b=0;b<$cont;b++){
+			var $createSpan = $("<span></span>")//创建
+			$createSpan.attr("index",b)	//设置非法属性
+			$createSpan.appendTo($(".lunbo").eq(a).find(".dot"))
+		}
+		$(".lunbo").eq(a).find(".dot span").eq(0).addClass("focus")
+		$(".lunbo").eq(a).find(".bigBox .box").eq(0).show()//默认显示box
 	}
-	$(".lunbo").eq(a).find(".dot span").eq(0).addClass("focus")
-	$(".lunbo").eq(a).find(".bigBox .box").eq(0).show()//默认显示box
-}
 
-//寻找当前的元素
-function seekElement(point){
-	$lunbo = $(point).closest(".lunbo")//找到当前的父级lunbo
-	$boxList = $lunbo.find(".bigBox .box")//找到当前的boxList集合
-	$dotList = $lunbo.find(".dot span")//找到当前的$squareList集合
-}
 
-// 向右点击的代码段
-function rightCode(){
-	for(var a=0;a<$boxList.length;a++){
-		if($boxList[a].style.display == "block"){
-			$boxList.eq(a).hide()
-			$dotList.eq(a).removeClass("focus")
-			$boxList.eq(a+1).fadeIn()
-			$dotList.eq(a+1).addClass("focus")
-			if(a == $boxList.length-1){
-				$boxList.eq(0).fadeIn()
-				$dotList.eq(0).addClass("focus")
+	//寻找当前的元素
+	function seekElement(point){
+		$lunbo = $(point).closest(".lunbo")//找到当前的父级lunbo
+		$boxList = $lunbo.find(".bigBox .box")//找到当前的boxList集合
+		$dotList = $lunbo.find(".dot span")//找到当前的$squareList集合
+	}
+
+	// 向右点击的代码段
+	function rightCode(){
+		for(var a=0;a<$boxList.length;a++){
+			if($boxList[a].style.display == "block"){
+				$boxList.eq(a).hide()
+				$dotList.eq(a).removeClass("focus")
+				$boxList.eq(a+1).fadeIn()
+				$dotList.eq(a+1).addClass("focus")
+				if(a == $boxList.length-1){
+					$boxList.eq(0).fadeIn()
+					$dotList.eq(0).addClass("focus")
+				}
+				return;
 			}
-			return;
 		}
 	}
-}
 
-$(".arrowsRight").click(function(){
-	seekElement(this)
-	rightCode()
-})
-
-$(".arrowsLeft").click(function(){
-	seekElement(this)
-	for(var a=0;a<$boxList.length;a++){
-		if($boxList[a].style.display == "block"){
-			$boxList.eq(a).hide()
-			$dotList.eq(a).removeClass("focus")
-			$boxList.eq(a-1).fadeIn()
-			$dotList.eq(a-1).addClass("focus")
-			if(a == 0){
-				$boxList.eq($boxList.length-1).fadeIn()
-				$dotList.eq($boxList.length-1).addClass("focus")
-			}
-			return;
-		}
-	}
-})
-
-// squareList点击事件
-$(".lunbo").find(".dot span").click(function(){
-	seekElement(this)
-	var n = $(this).index()
-	$boxList.hide()
-	$dotList.removeClass("focus")
-	$boxList.eq(n).fadeIn()
-	$dotList.eq(n).addClass("focus")
-})
-
-// 第一个大盒子自动播放
-function autoPlay(){
-	play = setInterval(function(){
-		$boxList = $(".lunbo").eq(0).find(".bigBox .box")//找到第一个轮播图的$boxList集合
-		$dotList = $(".lunbo").eq(0).find(".dot span")//找到第一个轮播图的$squareList集合
+	$(".arrowsRight").click(function(){
+		seekElement(this)
 		rightCode()
-	},3000)
-}
-autoPlay()
-$(".lunbo").eq(0).mouseenter(function(){
-	clearInterval(play)
-})
-$(".lunbo").eq(0).mouseleave(function(){
-	autoPlay()	
-})
-
-// 第一个轮播图右边的小图片，鼠标移上主要核心
-$(".lunbo").eq(0).find(".bigBox.one .box").mouseenter(function(){
-	var $imgImgList = $(this).find(".img img")	//找到中间的大图片
-	var $pickureImgList = $(this).find(".picture a")	//找到右侧的小图片
-	$pickureImgList.mouseenter(function(){
-		var p = $(this).index()
-		$imgImgList.closest(".img").css({background:"none"})
-		$imgImgList.hide()
-		//动画队列问题，JQuery内部提供了停止动画队列的方法 “ stop() ”
-		//问题描述：不停地快速划过$somalList并且停止时，之前的动画尚未执行完成，隐藏无效，会多出现几个你不想让他出现的怪东西
-		for(var a=0; a<$imgImgList.length;a++){
-			$imgImgList.eq(a).stop()
-		}
-		$imgImgList.eq(p).fadeIn()
 	})
-	$pickureImgList.mouseleave(function(){
-		$imgImgList.hide()
-		$imgImgList.closest(".img").css({background:""})
-	})
-})
 
-
-//鼠标移上出现弹框  代码段
-// age3: 鼠标要经过的对象集合
-// age4: 要显示的区块名(String)
-// age5: 现实的区块里面的要自动播放的图片集合(string)
-function  revealPopover(age3,age4,age5){
-	var x=0;
-	age3.mouseenter(function(){
-		//让右边section小区块显示
-		$(this).find(age4).fadeIn()
-		//获取四张图片
-		var $fourList = $(this).find(age4).find(age5)
-		//小图片计时器，永不停息
-		somalImg = setInterval(function(){
-			$fourList.hide()
-			if(x==$fourList.length-1){
-				x = 0
-			}else{
-				x++
+	$(".arrowsLeft").click(function(){
+		seekElement(this)
+		for(var a=0;a<$boxList.length;a++){
+			if($boxList[a].style.display == "block"){
+				$boxList.eq(a).hide()
+				$dotList.eq(a).removeClass("focus")
+				$boxList.eq(a-1).fadeIn()
+				$dotList.eq(a-1).addClass("focus")
+				if(a == 0){
+					$boxList.eq($boxList.length-1).fadeIn()
+					$dotList.eq($boxList.length-1).addClass("focus")
+				}
+				return;
 			}
-			$fourList.eq(x).fadeIn(200)
-		},2000)
+		}
 	})
-}
-//鼠标离开隐藏弹框  代码段
-// age3: 鼠标要经过的对象集合
-// age4: 要显示的区块名(String)
-function  concealPopover(age3,age4){
-	age3.mouseleave(function(){
-		$(this).find(age4).fadeOut(300)
-		$(this).mouseleave(function(){
-			$(this).find(age4).fadeOut()
-		})
-		clearInterval(somalImg)
-	})
-}
 
-$pass = $(".lunbo .bigBox .box").find(".section").parent()
-$section = $pass.find(".section")
-$fourList = $section.find(".four img")
-revealPopover($pass,$section,$fourList)
-concealPopover($pass,$section)
+	// squareList点击事件
+	$(".lunbo").find(".dot span").click(function(){
+		seekElement(this)
+		var n = $(this).index()
+		$boxList.hide()
+		$dotList.removeClass("focus")
+		$boxList.eq(n).fadeIn()
+		$dotList.eq(n).addClass("focus")
+	})
+
+	// 第一个大盒子自动播放
+	function autoPlay(){
+		play = setInterval(function(){
+			$boxList = $(".lunbo").eq(0).find(".bigBox .box")//找到第一个轮播图的$boxList集合
+			$dotList = $(".lunbo").eq(0).find(".dot span")//找到第一个轮播图的$squareList集合
+			rightCode()
+		},3000)
+	}
+	autoPlay()
+	$(".lunbo").eq(0).mouseenter(function(){
+		clearInterval(play)
+	})
+	$(".lunbo").eq(0).mouseleave(function(){
+		autoPlay()	
+	})
+
+	// 第一个轮播图右边的小图片，鼠标移上主要核心
+	$(".lunbo").eq(0).find(".bigBox.one .box").mouseenter(function(){
+		var $imgImgList = $(this).find(".img img")	//找到中间的大图片
+		var $pickureImgList = $(this).find(".picture a")	//找到右侧的小图片
+		$pickureImgList.mouseenter(function(){
+			var p = $(this).index()
+			$imgImgList.hide()
+			//动画队列问题，JQuery内部提供了停止动画队列的方法 “ stop() ”
+			//问题描述：不停地快速划过$somalList并且停止时，之前的动画尚未执行完成，隐藏无效，会多出现几个你不想让他出现的怪东西
+			for(var a=0; a<$imgImgList.length;a++){
+				$imgImgList.eq(a).stop()
+			}
+			$imgImgList.eq(p).fadeIn()
+		})
+		$pickureImgList.mouseleave(function(){
+			$imgImgList.hide()
+			console.log(fourImg[0])
+			$imgImgList.eq(0).fadeIn() //让第一个默认显示
+		})
+	})
+
+
+	//鼠标移上出现弹框  代码段
+	// age3: 鼠标要经过的对象集合
+	// age4: 要显示的区块名(String)
+	// age5: 现实的区块里面的要自动播放的图片集合(string)
+	function  revealPopover(age3,age4,age5){
+		var x=0;
+		age3.mouseenter(function(){
+			//让右边section小区块显示
+			$(this).find(age4).fadeIn()
+			//获取四张图片
+			var $fourList = $(this).find(age4).find(age5)
+			//小图片计时器，永不停息
+			somalImg = setInterval(function(){
+				$fourList.hide()
+				if(x==$fourList.length-1){
+					x = 0
+				}else{
+					x++
+				}
+				$fourList.eq(x).fadeIn(200)
+			},2000)
+		})
+	}
+	//鼠标离开隐藏弹框  代码段
+	// age3: 鼠标要经过的对象集合
+	// age4: 要显示的区块名(String)
+	function  concealPopover(age3,age4){
+		age3.mouseleave(function(){
+			$(this).find(age4).fadeOut(300)
+			$(this).mouseleave(function(){
+				$(this).find(age4).fadeOut()
+			})
+			clearInterval(somalImg)
+		})
+	}
+
+	$pass = $(".lunbo .bigBox .box").find(".section").parent()
+	$section = $pass.find(".section")
+	$fourList = $section.find(".four img")
+	revealPopover($pass,$section,$fourList)
+	concealPopover($pass,$section)
+}
 
 
 //*******************选项卡   新品与热门商品************************
@@ -219,6 +222,7 @@ function xianshi(obj){
 	$(obj).closest(".leftcol").find(".somalimgs i").eq(adv.m).addClass("baiBian")
 	$(obj).closest(".leftcol").find(".bigimgs img").eq(adv.m).fadeIn()
 }
+
 $(".sliderRight").click(function(){
 	++adv.m
 	var _this = this
@@ -247,7 +251,7 @@ $(".sliderLeft").click(function(){
 		$(".allsomalimgs").animate({
 			"marginLeft": 0
 		});
-		$(".huakuai").animate({
+		$(".huakuai").eq(0).animate({
 			left : 0 + "px"
 		})
 	}
@@ -263,7 +267,7 @@ function moveView(newIndex){
 		});
 
 		percent = (adv.m * 120 /(allsomalimgsWidth-600))
-		$(".huakuai").animate({
+		$(".huakuai").eq(0).animate({
 			left : Math.round((sliderWidth-huakuaiWidth)*percent) + "px"
 		})
 	}
@@ -275,7 +279,7 @@ function moveView(newIndex){
 
 			percent = ((adv.count - 5) * 120 / (allsomalimgsWidth-600))
 
-			$(".huakuai").animate({
+			$(".huakuai").eq(0).animate({
 				left : Math.round((sliderWidth-huakuaiWidth)*percent) + "px"
 			})
 		}
@@ -336,23 +340,12 @@ function onclckeMoveBox(){
 			"marginLeft": -(adv.count-5)*120 + "px"
 		});
 	}
-
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
 //滑块逻辑段
-$(".huakuai")[0].onmousedown = function(event){
+$(".huakuai").eq(0).mousedown(function(event){
 	var downX = event.clientX
 	distanceLeft = downX - parseInt(this.style.left)//left间距
 
@@ -384,67 +377,79 @@ $(".huakuai")[0].onmousedown = function(event){
 		}
 	}
 
-}
+})
 
 
 
 
-
+//三张小图片轮播图
 $(".chanpinThree").css({
 	width:205 * $(".threeBox").length
 })
-
-var chanpin = 0
-
-
-
-$(".sliderRight").click(function(){
-	var ThreeWidth =  $(this).closest(".kuai").find(".chanpinThree").width()
-	var threeBoxlength = $(this).closest(".kuai").find(".threeBox").length-1
+var chanpin = 0  //位置
+//点击逻辑
+function threeImg(here){
+	var ThreeWidth =  parseInt($(here).closest(".kuai").find(".chanpinThree").width())
+	var threeBoxlength = $(here).closest(".kuai").find(".threeBox").length-1
 	var chanpinLeft = parseInt($(".chanpinThree")[0].style.marginLeft)
-	console.log(chanpin++)
-
+	if(chanpin==-1){
+		chanpin = threeBoxlength-2
+	}
 	if(threeBoxlength-chanpin <2){
 		chanpin = 0
 	}
 	$(".chanpinThree").animate({
 		marginLeft : -chanpin*205
-		// $(".huakuai")[1].style.left = 
-		// parseInt($(".sliderAction").width()) 
-
 	})
-	console.log(parseInt($(".chanpinThree")[0].style.marginLeft)/(ThreeWidth-616))
-	// console.log(Math.round(parseInt($(".chanpinThree")[0].style.marginLeft)/ThreeWidth))
-
-
-
-
-
+	$(".huakuai").eq(1).animate({
+		left: - parseInt((-chanpin*205)/(ThreeWidth-616)*100)/100*(sliderWidth-huakuaiWidth)
+	})
+}
+//左右点击逻辑
+$(".sliderRight").click(function(){
+	var _this =  this
+	chanpin++
+	threeImg(_this)
 })
-
 $(".sliderLeft").click(function(){
+	var _this =  this
+	chanpin--
+	threeImg(_this)
 })
 
-// $(".huakuai")[2].onmousedown = function(event){
-
-// }
 
 
+$(".huakuai").eq(1).mousedown(function(event){
+	var downX = event.clientX
+	distanceLeft = downX - parseInt(this.style.left)//left间距
+	$("body")[0].onselectstart = function(){return false}
+	var _this = this
+	window.onmousemove = function(event){
+		movex = event.clientX
+		_this.style.left = movex - distanceLeft + "px"
+		//边缘检测				
+		if(parseInt(_this.style.left)<0){
+			_this.style.left = 0 + "px"
+		}
+		if(parseInt(_this.style.left)>sliderWidth - huakuaiWidth){
+			_this.style.left = sliderWidth - huakuaiWidth + "px"
+		}
+		//百分比
+		percent = parseInt(_this.style.left)/(sliderWidth-huakuaiWidth)
+		// left值
+		$(".chanpinThree")[0].style.left = - Math.round(($(".chanpinThree").width()-616) * percent) + "px"
+	}
+	window.onmouseup = function(event){
+		movex = event.clientX
+		distanceLeft = movex - parseInt(_this.style.left)//left间距
+		_this.style.left = movex - distanceLeft + "px"
+		window.onmousemove = null
+		$(".huakuai")[1].onmouseleave = function(){
+			$("body")[0].onselectstart = function(){return}
+		}
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 
 
 
---
+++ request.js
@@ -0,0 +1,116 @@
+
+var scr = document.createElement('script')
+scr.setAttribute("src","http://192.168.1.100:81?callback=callbackfn")
+document.getElementsByTagName("head")[0].appendChild(scr)
+
+var bigBoxOne= document.querySelectorAll('.bigBox.one')[0]//第一个轮播图
+var originalBox = bigBoxOne.querySelectorAll(".box")[0]//原版
+
+
+function callbackfn(data){
+	for(var i=0;i<data.length;i++){
+		fourImg = data[i].imgUrl // 数据里面对象的长度
+		var newBox = originalBox.cloneNode(true)//克隆新的
+		var somalImg = newBox.querySelectorAll(".picture a img")//右侧小图片
+		newBox.querySelectorAll(".url")[0].setAttribute("href",data[i].url) //游戏详情页路径
+		newBox.querySelectorAll(".url img")[0].style.display = "block"  //让第一个默认显示
+		//插入图片路径
+		for(j=0;j<fourImg.length;j++){
+			newBox.querySelectorAll(".picture a")[j].setAttribute("href",data[i].url)
+			newBox.querySelectorAll(".url img")[j].setAttribute("src",fourImg[j]) //中间的4张图片
+			somalImg[j].setAttribute("src",fourImg[j])  //右侧的4张小图片
+			newBox.querySelectorAll(".section .four img")[j].setAttribute("src",fourImg[j]) //弹出框4张图片
+		}
+		// 右边的图文混合区
+		newBox.querySelectorAll(".gameName")[0].innerHTML = data[i].name   //游戏名字
+		newBox.querySelectorAll(".original")[0].innerHTML = "￥" + data[i].originPrice //原价
+		newBox.querySelectorAll(".current")[0].innerHTML = "￥" + data[i].price //促销价
+		//小图标 mac win等
+		for(var j=0;j<data[i].platform.length;j++){
+			if(data[i].platform[j]=="Windows"){
+				newBox.querySelectorAll(".xiaotubiao span")[j].className ="win"
+			}
+			if(data[i].platform[j]=="Mac OS"){
+				newBox.querySelectorAll(".xiaotubiao span")[j].className = "mac"
+			}
+
+			if(data[i].platform[j]=="Steam"){
+				newBox.querySelectorAll(".xiaotubiao span")[j].className = "linux"
+			}
+		}
+		// 弹出框部分
+		newBox.querySelectorAll(".section .first")[0].innerHTML = data[i].name //游戏名字
+		// 字符串处理
+		var YTD = ["年","月","日"]
+		var figure = data[i].date.split("-")  //字符串从"-"分割截取,成为数组
+		var strdata = ""  //字符串日期
+		for(var j=0;j<figure.length;j++){
+			strdata = strdata + figure[j] + YTD[j]
+		}
+		newBox.querySelectorAll(".section .date")[0].innerHTML = strdata //日期
+
+		var appraise = ["好评如潮","特别好评","多半好评","褒贬不一","多半差评","差评如潮","无评论"]
+		newBox.querySelectorAll(".section .estimate")[0].innerHTML = appraise[data[i].evaluate-1]  //评价
+		console.log(data[i].evaluate)
+
+		//判断颜色
+		if(data[i].evaluate<4){
+			// console.log("hahaha")
+			newBox.querySelectorAll(".section .estimate")[0].style.color = "#00FFFF" 
+		}
+		if(data[i].evaluate==4){
+			newBox.querySelectorAll(".section .estimate")[0].style.color = "#7CFC00" 
+		}	
+		if(data[i].evaluate>4){		
+			newBox.querySelectorAll(".section .estimate")[0].style.color = "#8C8C8C"
+		}
+		//评价数量天加“,” 3个数字一格
+		var amount = String(data[i].evaluatingCount) //数字转字符串
+		function appendComma(str){
+			if(str.length<=3){
+				return str
+			}
+			else{
+				return appendComma(str.substr(0,str.length-3)) + "," + str.substr(str.length-3)
+			}
+		}
+		
+		newBox.querySelectorAll(".section .amount")[0].innerHTML = appendComma(amount)//评价数量
+
+		//弹出框小标签
+		var label = newBox.querySelectorAll(".section .biaoqian span")[0]//标签模板
+		for(var j=0;j<data[i].label.length;j++){
+			var newlabel = label.cloneNode() //克隆新的标签
+			newlabel.innerHTML = data[i].label[j]  //给标签赋值
+			label.parentNode.appendChild(newlabel) // 插入到(biaoqian)标签里面
+		}
+		label.parentNode.removeChild(label) //删除模板标签
+		bigBoxOne.appendChild(newBox) //将克隆出来的box插入进去
+	}
+
+	originalBox.parentNode.removeChild(originalBox)
+	luoji()//执行逻辑
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
